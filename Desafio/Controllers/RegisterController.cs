b68b76a713640f2d188d808ade70e9f538f4e6d8
Fernando A. Novara <Fernando_Novara@hotmail.com>
2023-05-09 21:31:49 -0300

    Desafio
    Se creo el segundo servicio

--- Desafio/Controllers/RegisterController.cs
+++ Desafio/Controllers/RegisterController.cs
@@ -29,35 +29,99 @@ public class RegisterController : ControllerBase
         }
     }
 
-//      Generar los servicios correspondientes, los cuales nos permitan guardar en base
-//      de datos, los ingresos y egresos del personal.
-//      Function register(idEmployee, date, registerType, businessLocation)
-
 
     [HttpPost("Register")]
-    public async Task<IActionResult> Register([FromForm]Register registro)
+    public async Task<IActionResult> Register([FromForm] Register registro)
     {
         try
         {
-            if(registro.RegisterType.Equals("Ingreso") && registro.RegisterType != null)
+            if (registro != null)
             {
-                registro.RegisterType = "Egreso";
-                registro.Date = DateTime.Now;
+                var res = await Contexto.register.OrderByDescending(x => x.Date).FirstOrDefaultAsync(x => x.IdEmployee == registro.IdEmployee);
+                if (res == null)
+                {
+                    if (registro.RegisterType.Equals("Ingreso"))
+                    {
+                        registro.Date = DateTime.Now;
+                        await Contexto.AddAsync(registro);
+                        await Contexto.SaveChangesAsync();
+                        
+                        return Ok(registro);
+                    }
+                    else
+                    {
+                        return BadRequest("El tipo de registro no puede puede ser egreso.");
+                    }
+                }
+                if (!registro.RegisterType.Equals(res.RegisterType))
+                {
+                    registro.Date = DateTime.Now;
+                    await Contexto.AddAsync(registro);
+                    await Contexto.SaveChangesAsync();
+
+                    return Ok(registro);
+                }
+                else
+                {
+                    if (registro.RegisterType.Equals("Ingreso"))
+                    {
+                        return BadRequest("No marco un egreso");
+                    }
+                    else
+                    {
+                        return BadRequest("No marco un Ingreso");
+                    }
+                }
             }
             else
             {
-                registro.RegisterType = "Ingreso";
-                registro.Date = DateTime.Now;
+                return BadRequest("No se puede cargar una registro nulo.");
             }
-
-            await Contexto.AddAsync(registro);
-            Contexto.SaveChanges();
-            return Ok(registro);
-        
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             return BadRequest(ex.Message);
         }
+    }
+
+//      Generar un servicio el cual liste la cantidad de ingresos y egresos dada una fecha
+//      desde – hasta, que se pueda filtrar por nombre o apellido y sucursal.
+//      Function search(dateFrom, dateTo , descriptionFilter,
+//      businessLocation)
+
+    [HttpGet("Search")]
+    public async Task<ActionResult<Register>> Search ([FromForm] String dateFrom,[FromForm] String dateTo,[FromForm] String? descriptionFilter,[FromForm] String? businessLocation)
+    {
+        List<Register> res;
+        if(!String.IsNullOrEmpty(descriptionFilter))
+        {
+            if(!String.IsNullOrEmpty(businessLocation))
+            {
+                res = await Contexto.register
+                .Include(e => e.employee)
+                .Include(b => b.business)
+                .Where(x => x.Date >= DateTime.Parse(dateFrom) && x.Date <= DateTime.Parse(dateTo) 
+                        && (x.employee.Nombre.Contains(descriptionFilter) || x.employee.Apellido.Contains(descriptionFilter)) 
+                        && (x.business.Location.Equals(businessLocation)))
+                .ToListAsync();
+            }
+            else{
+                res = await Contexto.register
+                .Include(e => e.employee)
+                .Include(b => b.business)
+                .Where(x => x.Date >= DateTime.Parse(dateFrom) && x.Date <= DateTime.Parse(dateTo) 
+                        && (x.employee.Nombre.Contains(descriptionFilter) || x.employee.Apellido.Contains(descriptionFilter)))
+                .ToListAsync();
+            }
+        }
+        else{
+                res = await Contexto.register
+                .Include(e => e.employee)
+                .Include(b => b.business)
+                .Where(x => x.Date >= DateTime.Parse(dateFrom) && x.Date <= DateTime.Parse(dateTo))
+                .ToListAsync();
+        }
+
+        return Ok(res);
     }
 }
--- Desafio/Models/Register.cs
+++ Desafio/Models/Register.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Desafio{
 
@@ -6,8 +7,14 @@ namespace Desafio{
         
         [Key]
         public int IdRegister {get;set;}
-        public int idEmployee {get;set;}
+        public int IdEmployee {get;set;}
+
+        [ForeignKey(nameof(IdEmployee))]
+        public Employee employee {get;set;}
         public int IdBusiness {get;set;}
+        
+        [ForeignKey(nameof(IdBusiness))]
+        public Business business {get;set;}
         public DateTime? Date {get;set;}
         public String? RegisterType {get;set;}
     }
